0cc2c133e89c81394a196da148030eaaf605f156
Stephanos Kuma <stephanos@kuma.ai>
2022-11-14 20:58:42 +0000

    Add a simple test for parsing

---
+++ tests/pvenv/subcommands/test_invenv.py
@@ -0,0 +1,12 @@
+import pytest
+
+from pvenv.subcommands.invenv import Command
+
+
+class TestInVenvCommand:
+    @staticmethod
+    @pytest.mark.parametrize(
+        ["line", "expected"], [["x=1", ["x", "1"]], ["x=a=1", ["x", "a=1"]]]
+    )
+    def test_parse_env_var(line, expected):
+        assert Command.parse_env_var(line) == expected
