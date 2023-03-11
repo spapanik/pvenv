66b6f375a4584e097aad463ced2f981633bb5551
Stephanos Kuma <stephanos@kuma.ai>
2023-03-11 23:37:50 +0000

    Add some more rules

--- tests/pvenv/subcommands/test_invenv.py
+++ tests/pvenv/subcommands/test_invenv.py
@@ -6,7 +6,7 @@ from pvenv.subcommands.invenv import Command
 class TestInVenvCommand:
     @staticmethod
     @pytest.mark.parametrize(
-        ["line", "expected"], [["x=1", ["x", "1"]], ["x=a=1", ["x", "a=1"]]]
+        ("line", "expected"), [("x=1", ["x", "1"]), ("x=a=1", ["x", "a=1"])]
     )
     def test_parse_env_var(line, expected):
         assert Command.parse_env_var(line) == expected
